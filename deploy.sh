bdf971337050304317151f8bcbf29e6c3fe8272c
sandeep-gupta-dontula <dontulasandeepgupta@gmail.com>
2024-06-27 22:30:49 -0400

    this is my last and last  commit in dev branch

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 adding some lines to ecixting hwefuqegfvuefhgvwekfh
-this file contains the deployment details 
+this file contains the degvhbhhhkjbk  m jnl deployment details 
 dded this line imn  dev branvj
